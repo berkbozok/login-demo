ed7a85695cf31281f7df78cf017f1a74f092540e
Berk Bozok <berk_bozok@live.com>
2022-05-09 06:25:59 -0400

    if else fixed

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -6,26 +6,26 @@ export const validateForm = values => {
   if (!values.email) {
     errors.email = 'Email is required!';
   } 
-  if (!values.password) {
+  else if (!values.password) {
     errors.password = 'Password is required!';
   } 
-  else if (!regex.test(values.email)) {
+   if (!regex.test(values.email)) {
     errors.email = 'This is not a valid email format!';
   }
   else if(values.email !== "admin@email.com"){
     errors.email = 'This email is not registered!';
   }
-
-  else if(values.password !== "admintest"){
-    errors.email = 'This password is incorrect!';
-  }
-
   else if (values.password.length < 4) {
     errors.password = 'Password must be more than 4 characters!';
   } 
   else if (values.password.length > 15) {
     errors.password = 'Password cannot exceed more than 15 characters!';
   }
+  else if(values.password !== "admintest"){
+    errors.email = 'This password is incorrect!';
+  }
+
+  
   
 
   return errors;
